1dc746767d943740128a3f0387b29d8f2d576149
baileyrachael25 <baileyrachael25@gmail.com>
2022-12-01 18:18:29 -0800

    started html and js files

---
+++ Assets/js/script.js
@@ -0,0 +1,7 @@
+//Weather dashboard script file
+
+//search btn
+var searchBtn = document.getElementById('search-btn').addEventListener('click', function(console.log('hi')))
+
+var cityName = document.getElementById('cityName').value
+console.log(cityName)
