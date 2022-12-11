922a4cb676d96af9e33057b5b3e70b26e07f482a
baileyrachael25 <baileyrachael25@gmail.com>
2022-12-11 14:52:34 -0800

    reorganize html file and adding styles

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,1 @@
-//Weather dashboard script file
-
-//search btn
-var searchBtn = document.getElementById('search-btn').addEventListener('click', function(console.log('hi')))
-
-var cityName = document.getElementById('cityName').value
-console.log(cityName)
+//variables
